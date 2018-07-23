6247c4e95c01d6cc5c10a027f5c80f871d757fab
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-23 13:33:31 -0700

    ADD empty class Dog.

---
+++ real_python_oop_tut.py
@@ -0,0 +1,8 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# from "Object-Oriented Programming (OOP) in Python 3"
+# https://realpython.com/python3-object-oriented-programming/
+# # "The Oldest Dog" review exercise 1
+class Dog:
+
+    pass
