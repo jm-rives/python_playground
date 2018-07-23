17b7852db3af124ca9a8c45613e5ce496ab9d67d
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-23 15:40:13 -0700

    RENAME file.

---
+++ pets_class.py
@@ -0,0 +1,36 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# Real Python Object Oriented programming tutorial
+# https://realpython.com/python3-object-oriented-programming/
+
+# Starter Code
+# Parent class
+class Dog:
+
+    # Class attribute
+    species = 'mammal'
+
+    # Initializer / Instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # instance method
+    def description(self):
+        return "{} is {} years old".format(self.name, self.age)
+
+    # instance method
+    def speak(self, sound):
+        return "{} says {}".format(self.name, sound)
+
+# Child class (inherits from Dog class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "{} runs {}".format(self.name, speed)
+
+# Child class (inherits from Dog class)
+class Bulldog(Dog):
+    def run(self, speed):
+        return "{} runs {}".format(self.name, speed)
+
+        
