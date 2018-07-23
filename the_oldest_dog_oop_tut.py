c53efbcae33e9e660908098818bcfc835252e571
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-23 14:56:42 -0700

    UPDATE file name.

---
+++ the_oldest_dog_oop_tut.py
@@ -0,0 +1,62 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# from "Object-Oriented Programming (OOP) in Python 3"
+# https://realpython.com/python3-object-oriented-programming/
+# # "The Oldest Dog" review exercise 1
+class Dog:
+    # Class Attribute
+    animal_class = "mammal"
+
+    # Initializer function with Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+def get_biggest_number(list_of_dogs):
+    """Accepts a list of Dog objects
+    and returns the max age of dogs in the list. """
+    dog_ages = []
+
+    for dog in dogs:
+        dog_ages.append(dog.age)
+        oldest = max(dog_ages)
+
+    print(f"The oldest dog is {oldest} years old.")
+
+
+dana = Dog("Dana", 44)
+balto = Dog("Balto", 50)
+mandy = Dog("Mandy", 55)
+
+dogs = [dana, balto, mandy]
+
+get_biggest_number(dogs)
+
+# # Example Solution
+# class Dog:
+#
+#     # Class Attribute
+#     species = 'mammal'
+#
+#     # Initializer / Instance Attributes
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# # Instantiate the Dog object
+# jake = Dog("Jake", 7)
+# doug = Dog("Doug", 4)
+# william = Dog("William", 5)
+#
+#
+# # Determine the oldest dog
+# def get_biggest_number(*args):
+#     return max(args)
+#
+#
+# # Output
+# print("The oldest dog is {} years old.".format(
+#     get_biggest_number(jake.age, doug.age, william.age)))
