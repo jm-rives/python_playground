f96d2027fc845bbd256d116e698afe2cf8911d5c
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-26 13:11:10 -0700

    ADD file encoding.

---
+++ dog_walking.py
@@ -0,0 +1,4 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# Real Python Object Oriented programming tutorial
+# https://realpython.com/python3-object-oriented-programming/
