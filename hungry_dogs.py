14778c80d6d491cad22c4a43f9f05cbc48bf4765
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-26 11:51:33 -0700

    FIX typo.

--- hungry_dogs.py
+++ hungry_dogs.py
@@ -17,7 +17,8 @@ class Dog:
 
     # Class attribute (changed to constant, science has not achieved trans-species tech, yet.)
     SPECIES = 'mammal'
-    is_hungery = True
+    is_hungry = True
+
     # Initializer / Instance attributes
     def __init__(self, name, age):
         self.name = name
